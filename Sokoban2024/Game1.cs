9b47cc727ea772a288d2d8fca052140c5f799e75
Francisco Gomes <flgomes2004@gmail.com>
2024-03-13 12:46:12 +0000

    Adicionado Sprites

--- Sokoban2024/Game1.cs
+++ Sokoban2024/Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.IO;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -9,6 +10,11 @@ public class Game1 : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
     private SpriteFont font;
+    private int nrLinhas = 0;
+    private int nrColunas = 0;
+    private char[,] level;
+    private Texture2D player, dot, box, wall; //Load images Texture 
+    int tileSize = 64; //potencias de 2 (operações binárias)    
 
     public Game1()
     {
@@ -20,6 +26,11 @@ public class Game1 : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
+        
+        LoadLevel("level1.txt");
+        _graphics.PreferredBackBufferHeight = tileSize * level.GetLength(1); //definição da altura
+        _graphics.PreferredBackBufferWidth = tileSize * level.GetLength(0); //definição da largura
+        _graphics.ApplyChanges(); //aplica a atualização da janela
 
         base.Initialize();
     }
@@ -28,8 +39,28 @@ public class Game1 : Game
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
         font = Content.Load<SpriteFont>("File"); //Use the name of the Sprite Font File ("File")
+        player = Content.Load<Texture2D>("Character4");
+        dot = Content.Load<Texture2D>("EndPoint_Blue");
+        box = Content.Load<Texture2D>("Crate_Purple");
+        wall = Content.Load<Texture2D>("Wall_Brown");
 
         // TODO: use this.Content to load your game content here
+    }
+
+    void LoadLevel(string levelFile)
+    {
+        string[] linhas = File.ReadAllLines($"Content/{levelFile}");
+        nrLinhas = linhas.Length;
+        nrColunas = linhas[0].Length;
+
+        level = new char[nrColunas, nrLinhas];
+        for (int x = 0; x < nrColunas; x++)
+        {
+            for (int y = 0; y < nrLinhas; y++)
+            {
+                level[x, y] = linhas[y][x];
+            }
+        }
     }
 
     protected override void Update(GameTime gameTime)
@@ -48,7 +79,34 @@ public class Game1 : Game
         GraphicsDevice.Clear(Color.Pink);
         
         _spriteBatch.Begin();
-        _spriteBatch.DrawString(font, "O texto que eu quiser", new Vector2(100, 100), Color.Blue);
+        //_spriteBatch.DrawString(font, "BALLS", new Vector2(100, 100), Color.Blue);
+        //_spriteBatch.DrawString(font, $"Numero de Linhas = {nrLinhas} -- Numero de Colunas = {nrColunas}",new Vector2(200, 200), Color.Blue);
+        
+        Rectangle position = new Rectangle(0, 0, tileSize, tileSize); //calculo do retangulo a depender do tileSize
+        for (int x = 0; x < level.GetLength(0); x++)  //pega a primeira dimensão
+        {
+            for (int y = 0; y < level.GetLength(1); y++) //pega a segunda dimensão
+            {
+                position.X = x * tileSize; // define o position
+                position.Y = y * tileSize; // define o position
+
+                switch (level[x, y])
+                {
+                    case 'Y':
+                        _spriteBatch.Draw(player, position, Color.White);
+                        break;
+                    case '#':
+                        _spriteBatch.Draw(box, position, Color.White);
+                        break;
+                    case '.':
+                        _spriteBatch.Draw(dot, position, Color.White);
+                        break;
+                    case 'X':
+                        _spriteBatch.Draw(wall, position, Color.White);
+                        break;
+                }
+            }
+        }
         _spriteBatch.End();
 
         // TODO: Add your drawing code here
